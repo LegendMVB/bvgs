aeea50104d685877e67bbf1d54a5cab078343ce4
Minh Bui <minh.bui@brreg.no>
2019-11-04 10:23:58 +0100

    View of the results

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Paho from 'paho-mqtt';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+var MQTT_HOST = "test.mosquitto.org"
+var MQTT_PORT = 8081
+var MQTT_TOPIC = "bvgs"
+
+
+var client = new Paho.Client(MQTT_HOST, MQTT_PORT, "clientId");
+
+
+client.connect({
+  useSSL: true,
+  onSuccess: function () {
+    console.log('connected to broker')
+    client.subscribe(MQTT_TOPIC)
+  }
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+  }
+
+  componentDidMount() {
+
+    client.onMessageArrived = (message) => {
+      console.log(message.payloadString)
+      this.setState(state => {
+        const list = state.list.concat(message.payloadString);
+        return {
+          list,
+        };
+      });
+      };
+  }
+
+  render() {
+    return (
+        <div className="App" >
+          <header className="App-header">
+            <h1>Oversikt over gruppenes besvarelser</h1>
+            <ol>
+              {this.state.list.map(item => (
+                  <li key={item}>{item}</li>
+              ))}
+            </ol>
+          </header>
+        </div>
+    )
+  }
 }
 
 export default App;
